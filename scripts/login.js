4bf6a0b773454c5e90659c8f726d93dee18c5f8e
barbieri97 <barbieri97@mail.com>
2023-02-06 18:33:56 -0300

    code refatoring

--- scripts/login.js
+++ scripts/login.js
@@ -1,21 +1,6 @@
-
-// adiciona o link css para o login
-var link = $('<link>').attr({
-    'rel': 'stylesheet',
-    'type': 'text/css',
-    'href': '/../styles/login.css'
-})
-$('head').append(link);
-
-$("title").append(" | Login")
-
-// adiciona html e js do index
-$.get('/../componentes/login.html', (data, status) => {
-    $('main').append(data);
-    $('.message a').click(function(){
-        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-     });
-    $('button').click(() => {
-        alert("em breve você poderá se cadastrar e aproveitar todos os beneficios online da nossa clínica!")
-        })
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+$('button').click(() => {
+    alert("em breve você poderá se cadastrar e aproveitar todos os beneficios online da nossa clínica!")
     })
