14fdb063c167dce9fc7b98bcb0d6ef669bc43cbd
barbieri97 <andrepb636@gmail.com>
2023-09-04 11:54:27 -0300

    add notification ntfy

--- scripts/main.js
+++ scripts/main.js
@@ -11,4 +11,19 @@ $('nav').animate({cursor: 'auto'}, 'slow');
 
 $("#loadingMask").fadeOut()
 $('header').css('display', 'flex')
+
+$.ajax({
+    url: "https://ntfy.barbiericlinica.com.br/siteclinica",
+    method: "POST",
+    data: 'um novo acesso a pagina barbiericlinica.com.br foi verificado',
+    headers: {'t': "Novo acesso a barbiericlinica.com.br"},
+    success: function(response) {
+        console.log("POST bem sucedido", response)
+    },
+    error: function(error) {
+        console.log("Erro no POST", error)
+    }
+})
+
+
 })
